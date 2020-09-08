212de41239b9df42413aab334fe1c1545a8ecc0d
Zac Burns <That3Percent@gmail.com>
2020-09-08 09:33:55 -0500

    Test: Better framework

--- src/tests.rs
+++ src/tests.rs
@@ -1,15 +1,35 @@
 use crate::*;
+use num_bigint::BigInt;
+
+trait IntoTestResult {
+    fn into(self) -> Result<BigInt, CostError>;
+}
+
+impl IntoTestResult for u64 {
+    fn into(self) -> Result<BigInt, CostError> {
+        Ok(BigInt::from(self))
+    }
+}
+
+impl IntoTestResult for CostError {
+    fn into(self) -> Result<BigInt, CostError> {
+        Err(self)
+    }
+}
+
+fn test(model: &str, query: &str, result: impl IntoTestResult) {
+    let model = CostModel::compile(model).unwrap();
+    let cost = model.cost(query);
+    assert_eq!(result.into(), cost);
+}
 
 #[test]
-fn test() {
+fn query_match() {
     let model = "
-        query { a } where false => 1;
+        query { a } where true => 11;
+        query { b } where false => 12;
         query { b } where 1 == 1 => 2 + 2;
     ";
-
-    let model = CostModel::compile(model).unwrap();
-
-    let cost = model.cost("query { b }");
-
-    assert_eq!(Ok(4.into()), cost);
+    test(model, "query { a }", 11);
+    test(model, "query { b }", 4);
 }
