236b0630413879dd1cdea96ed84bb59a92a1cdf7
Jannis Pohlmann <jannis@edgeandnode.com>
2024-05-10 22:16:07 +0200

    feat: allow to create contexts with a static lifetime

--- lang/src/context.rs
+++ lang/src/context.rs
@@ -25,6 +25,29 @@ impl<'a, T: q::Text<'a>> Context<'a, T> {
     }
 }
 
+impl Context<'static, String> {
+    /// Create a context whose lifetime does not depend on its inputs.
+    ///
+    /// This is useful in situations where the context is kept around beyond the
+    /// lifetime of the input query and variables.
+    pub fn new_static(query: &str, variables: &str) -> Result<Self, CostError> {
+        profile_method!(new_static);
+
+        let parsed_variables =
+            crate::parse_vars(&variables).map_err(|_| CostError::FailedToParseVariables)?;
+        let query = q::parse_query(&query)
+            .map_err(|_| CostError::FailedToParseQuery)?
+            .into_static();
+        let (operations, fragments) = crate::split_definitions(query.definitions);
+
+        Ok(Self {
+            variables: parsed_variables,
+            fragments,
+            operations,
+        })
+    }
+}
+
 impl<'a, T: q::Text<'a> + Clone> Clone for Context<'a, T> {
     fn clone(&self) -> Self {
         Self {
